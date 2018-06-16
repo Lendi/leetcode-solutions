fa764fb6f4fd363ad397107d5f2960d86cf1cae5
Lendi <lendivihari.com>
2018-06-16 22:51:17 +0530

    Test

--- dynamic-programming/IsSubsequence.java
+++ dynamic-programming/IsSubsequence.java
@@ -19,6 +19,7 @@ Follow up:
 If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
 
 */
+//test
 
 class Solution {
     public boolean isSubsequence(String s, String t) {
