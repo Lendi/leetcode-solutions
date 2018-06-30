efc3de35f8bca083f2f4f9cf91dc37b81562a3f8
Lendi <lendivihari@gmail.com>
2018-06-30 23:59:40 +0530

    Max subarray - DP

--- dynamic-programming/MaxSubarray.java
+++ dynamic-programming/MaxSubarray.java
@@ -13,7 +13,6 @@ public class Solution {
     public int maxSubArray(int[] nums) {
 
         int max = Integer.MIN_VALUE, sum = 0;
-
         for(int i = 0; i < nums.length; i++)
         {
             if(sum < 0)
